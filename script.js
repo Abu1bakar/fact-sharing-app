ad1045ae54287c183fe61d520dd4598ea00dedfa
Abubakar <hussainabubakar698@gmail.com>
2025-12-12 16:47:23 +0200

    Fixed

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ console.log(logicalVotes);
 console.log(logicalVotes2);
 console.log(totalValue);
 
-*/
+
 
 // Function
 function calcYear(year) {
@@ -58,3 +58,63 @@ const totalValue = upVote + MindBlowing;
 const message =
   totalValue > falseFact ? 'The fact is True' : 'The Fact is false';
 console.log(message);
+
+const factYearAge = (year) =>
+  year <= new Date().getFullYear()
+    ? `${year} was ${new Date().getFullYear() - year} years ago`
+    : `We will see this year in ${year - new Date().getFullYear()} years time`;
+
+console.log(factYearAge(2028));
+console.log(factYearAge(2015));
+
+
+//Arrays
+
+const fact = ['React was created by facebook', 2016, true];
+
+const [text, createdIn, isCorrect] = fact;
+
+console.log(text);
+console.log(createdIn);
+console.log(isCorrect);
+
+const newFact = [...fact, 'Socity'];
+console.log(newFact);
+
+const newUsers = ['Abubakar', 'Amanda', 'Jolly', 'Adam'];
+
+const [Admin, ...restUsers] = newUsers;
+
+console.log(newUsers);
+console.log(Admin);
+console.log(restUsers);
+
+const userUpdates = [...newUsers, 'Katie', 'saif', 'Mohammed'];
+
+console.log(userUpdates);
+
+console.log(userUpdates[3]);
+
+*/
+
+const factObj = {
+  text: 'Tiktok Is The Fastest Growing Business Booster as of 2025 ',
+  category: 'Entertainment',
+  createdIn: 2025,
+  isCorrect: true,
+  currentSummary: function () {
+    return `This fact is ${
+      this.text
+    }and from the category of ${this.category.toUpperCase()}`;
+  },
+};
+
+console.log(factObj.text);
+
+const { text, createdIn, isCorrect } = factObj;
+
+console.log(text);
+console.log(createdIn);
+console.log(isCorrect);
+
+console.log(factObj.currentSummary());
